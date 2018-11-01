780def7b9ae09aa60fa82bd499d2027b342df796
GeorgeFucker <33426483+GeorgeFucker@users.noreply.github.com>
2018-11-01 15:07:53 +0200

    Summarizer and ReadTime, Readability estimation are added.

--- matcher/Tagger.py
+++ matcher/Tagger.py
@@ -1,5 +1,7 @@
 import textacy
 import nltk
+import bs4
+import requests
 
 import textacy.keyterms as tkt
 
@@ -8,10 +10,30 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from textacy import similarity
 from functools import reduce
+from textstat.textstat import textstat
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer1
+# from sumy.summarizers.text_rank import TextRankSummarizer as Summarizer2
+# from sumy.summarizers.luhn import LuhnSummarizer as Summarizer3
+# from sumy.summarizers.lsa import LsaSummarizer as Summarizer4
+# from sumy.summarizers.sum_basic import SumBasicSummarizer as Summarizer5
+# from sumy.summarizers.kl import KLSummarizer as Summarizer6
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 
 
 class Tagger:
     """ Tagger class """
+
+    # Words per minute
+    WPM = 266
+    WORD_LENGTH = 5
+    IMG_WEIGHT = 0.15
+
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 4
 
     def __init__(self, tags, to_proprocess=True, algorithm='sgrank', to_exclude=None, **kwargs):
         """ Initialize with given parameters """
@@ -56,19 +78,11 @@ class Tagger:
     def parse(self, url):
         """ Get text from url """
 
-        try:
-            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-            webpage = urlopen(req).read()
-        except:
-            print('Something wrong')
-
-        soup = BeautifulSoup(webpage, 'html.parser')
-
-        # Kill all scripts and style elements
-        for script in soup(['script', 'style']):
-            script.extract()
-
-        return soup.get_text()
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        soup = bs4.BeautifulSoup(webpage, 'html.parser')
+        text = soup.findAll(text=True)
+        return text
 
     def preprocess(self, lowercase=False, no_punct=False, no_urls=False, no_stop_words=False):
         """ Preprocess text for matching """
@@ -162,3 +176,56 @@ class Tagger:
             matches = {(match, p) for match, p in matches if match not in self.to_exclude}
 
         return matches
+
+    ### Readtime and Readability estimation part
+    def _is_visible(self, element):
+        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif isinstance(element, bs4.element.Comment):
+            return False
+        elif element.string == "\n":
+            return False
+        return True
+
+    # filter all unimportant data
+    def _filter_visible_text(self, page_texts):
+        return filter(self._is_visible, page_texts)
+
+    def _count_words_in_text_through_average_word_length(self, text_list, word_length):
+        total_words = 0
+        for current_text in text_list:
+            total_words += len(current_text) / word_length
+        return total_words
+
+    def _get_img_count(self, url):
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+        return len(soup.find_all('img'))
+
+    # actual estimation
+    def estimate_readtime_and_readability(self, url):
+        filtered_text = self._filter_visible_text(self.text)
+        total_words = self._count_words_in_text_through_average_word_length(filtered_text, self.WORD_LENGTH)
+        img_count = self._get_img_count(url)
+        text_readability = textstat.flesch_reading_ease(' '.join(list(self._filter_visible_text(self.text))))
+        if text_readability > 70.0:
+            readability_res = 'this is an easy to read article'
+        elif 70.0 > text_readability > 60.0:
+            readability_res = 'this article is easy to read even for a beginner'
+        elif 60.0 > text_readability > 50.0:
+            readability_res = 'this is an article for student with some knowledge'
+        elif 50.0 > text_readability > 30.0:
+            readability_res = 'this is an article for student with strong knowledge of basics'
+        elif text_readability < 30.0:
+            readability_res = 'this is a relatively hard to read article'
+        return round((total_words / self.WPM) + img_count * self.IMG_WEIGHT), readability_res
+
+    ### Summarizer part
+    def summarize(self, url, func=Summarizer1):
+        parser = HtmlParser.from_url(url, Tokenizer(self.LANGUAGE))
+        stemmer = Stemmer(self.LANGUAGE)
+        summarizer = func(stemmer)
+        summarizer.stop_words = get_stop_words(self.LANGUAGE)
+        for sentence in summarizer(parser.document, self.SENTENCES_COUNT):
+            print(sentence)
+
